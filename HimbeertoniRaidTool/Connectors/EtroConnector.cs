1d700d13a988aa5f9c309f3f153ab4f8a614c790
Koenari <Koenari@koenari.de>
2022-02-12 15:19:20 +0100

    Preparation for more Etro Functions

--- HimbeertoniRaidTool/Connectors/EtroConnector.cs
+++ HimbeertoniRaidTool/Connectors/EtroConnector.cs
@@ -27,10 +27,73 @@ namespace HimbeertoniRaidTool.Connectors
             EtroGearItem? etroResponse = JsonConvert.DeserializeObject<EtroGearItem>(myJsonResponse);
             if (etroResponse == null)
                 return false;
-            item.name = etroResponse.name;
-            item.description = etroResponse.description;
+            item.name = etroResponse.name == null ? "" : etroResponse.name;
+            item.description = etroResponse.description == null ? "" : etroResponse.description;
             item.itemLevel = etroResponse.itemLevel;
             return true;
         }
+    }
+    class EtroGearItem
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public int param0 { get; set; }
+        public int param1 { get; set; }
+        public int param2 { get; set; }
+        public int param3 { get; set; }
+        public object? param4 { get; set; }
+        public object? param5 { get; set; }
+        public int param0Value { get; set; }
+        public int param1Value { get; set; }
+        public int param2Value { get; set; }
+        public int param3Value { get; set; }
+        public int param4Value { get; set; }
+        public int param5Value { get; set; }
+        public MaxParams? maxParams { get; set; }
+        public bool advancedMelding { get; set; }
+        public int block { get; set; }
+        public int blockRate { get; set; }
+        public bool canBeHq { get; set; }
+        public int damageMag { get; set; }
+        public int damagePhys { get; set; }
+        public int defenseMag { get; set; }
+        public int defensePhys { get; set; }
+        public int delay { get; set; }
+        public int iconId { get; set; }
+        public string? iconPath { get; set; }
+        public int itemLevel { get; set; }
+        public object? itemSpecialBonus { get; set; }
+        public int itemSpecialBonusParam { get; set; }
+        public int level { get; set; }
+        public int materiaSlotCount { get; set; }
+        public int materializeType { get; set; }
+        public bool PVP { get; set; }
+        public int rarity { get; set; }
+        public int slotCategory { get; set; }
+        public bool unique { get; set; }
+        public bool untradable { get; set; }
+        public bool weapon { get; set; }
+        public bool canCustomize { get; set; }
+        public string? slotName { get; set; }
+        public string? jobName { get; set; }
+        public int itemUICategory { get; set; }
+        public int jobCategory { get; set; }
+    }
+    class MaxParams
+    {
+        public int _6 { get; set; }
+        public int _10 { get; set; }
+        public int _11 { get; set; }
+        public int _19 { get; set; }
+        public int _22 { get; set; }
+        public int _27 { get; set; }
+        public int _44 { get; set; }
+        public int _45 { get; set; }
+        public int _46 { get; set; }
+        public int _70 { get; set; }
+        public int _71 { get; set; }
+        public int _72 { get; set; }
+        public int _73 { get; set; }
     }
 }
