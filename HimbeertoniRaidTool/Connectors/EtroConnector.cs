bfcc740ce5d775d3b517551714b4a634d8db876a
Koenari <Koenari@koenari.de>
2022-02-13 19:24:11 +0100

    Adding Capability to get Gearsets

--- HimbeertoniRaidTool/Connectors/EtroConnector.cs
+++ HimbeertoniRaidTool/Connectors/EtroConnector.cs
@@ -1,30 +1,39 @@
-﻿using HimbeertoniRaidTool.Data;
+﻿using Dalamud.Logging;
+using HimbeertoniRaidTool.Data;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HimbeertoniRaidTool.Connectors
 {
-    class EtroConnector : GearConnector
+    public class EtroConnector : GearConnector
     {
-        private readonly String BaseUri;
+        private const string BaseUri = "https://etro.gg/api/";
+        private string EquipmentBaseUri { get => (BaseUri + "equipment/"); }
+        private string GearsetBaseUri { get => (BaseUri + "gearsets/"); }
         private WebClient Client;
         public EtroConnector() {
-            BaseUri = "https://etro.gg/api/equipment/";
             Client = new WebClient();
             Client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
         }
-
-        async Task<bool> GearConnector.GetGearStats(GearItem item)
+        public async Task<bool> GetGearStats(GearItem item)
         {
-            int itemID = item.GetID();
-            string myJsonResponse = await new StreamReader(Client.OpenRead(BaseUri + itemID)).ReadToEndAsync();
-            EtroGearItem? etroResponse = JsonConvert.DeserializeObject<EtroGearItem>(myJsonResponse);
+            if (item.ID < 1)
+                return false;
+            EtroGearItem? etroResponse;
+            try
+            {
+                string jsonResponse = await new StreamReader(Client.OpenRead(EquipmentBaseUri + item.ID)).ReadToEndAsync();
+                etroResponse = JsonConvert.DeserializeObject<EtroGearItem>(jsonResponse);
+            } catch (WebException e)
+            {
+                PluginLog.LogError(e.Message);
+                return false;
+            }
+            
             if (etroResponse == null)
                 return false;
             item.name = etroResponse.name == null ? "" : etroResponse.name;
@@ -32,6 +41,78 @@ namespace HimbeertoniRaidTool.Connectors
             item.itemLevel = etroResponse.itemLevel;
             return true;
         }
+
+        public async Task<bool> GetGearSet(GearSet set)
+        {
+            string id = set.EtroID;
+            if (id.Equals(""))
+                return false;
+            EtroGearSet? etroSet;
+            try
+            {
+                string jsonResponse = await new StreamReader(Client.OpenRead(GearsetBaseUri + id)).ReadToEndAsync();
+                etroSet = JsonConvert.DeserializeObject<EtroGearSet>(jsonResponse);
+            }
+            catch (WebException e)
+            {
+                PluginLog.LogError(e.Message);
+                return false;
+            }
+            
+            if (etroSet == null)
+                return false;
+            set.Weapon = new(etroSet.weapon);
+            set.Head = new(etroSet.head);
+            set.Head = new(etroSet.head);
+            set.Body = new(etroSet.body);
+            set.Gloves = new(etroSet.hands);
+            set.Legs = new(etroSet.legs);
+            set.Feet = new(etroSet.feet);
+            set.Earrings = new(etroSet.ears);
+            set.Necklace = new(etroSet.neck);
+            set.Bracelet = new(etroSet.wrists);
+            set.Ring1 = new(etroSet.fingerL);
+            set.Ring2 = new(etroSet.fingerR);
+            set.OffHand = new(etroSet.offHand);
+            await set.FillStats(this);
+            return true;
+        }
+
+    }
+    class EtroGearSet
+    {
+        public string? id { get; set; }
+        public string? jobAbbrev { get; set; }
+        public string? jobIconPath { get; set; }
+        public string? clanName { get; set; }
+        public bool isOwner { get; set; }
+        public string? name { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public int minItemLevel { get; set; }
+        public int maxItemLevel { get; set; }
+        public int minMateriaTier { get; set; }
+        public int maxMateriaTier { get; set; }
+        //public Materia materia { get; set; }
+        public List<TotalParam>? totalParams { get; set; }
+        //public object buffs { get; set; }
+        //public object relics { get; set; }
+        public int patch { get; set; }
+        public int job { get; set; }
+        public int clan { get; set; }
+        public int weapon { get; set; }
+        public int head { get; set; }
+        public int body { get; set; }
+        public int hands { get; set; }
+        public int legs { get; set; }
+        public int feet { get; set; }
+        public int offHand { get; set; }
+        public int ears { get; set; }
+        public int neck { get; set; }
+        public int wrists { get; set; }
+        public int fingerL { get; set; }
+        public int fingerR { get; set; }
+        public int food { get; set; }
+        public int medicine { get; set; }
     }
     class EtroGearItem
     {
@@ -95,5 +176,12 @@ namespace HimbeertoniRaidTool.Connectors
         public int _71 { get; set; }
         public int _72 { get; set; }
         public int _73 { get; set; }
+    }
+    class TotalParam
+    {
+        public int? id { get; set; }
+        public string? name { get; set; }
+        public decimal value { get; set; }
+        public string? units { get; set; }
     }
 }
--- HimbeertoniRaidTool/Connectors/GearConnector.cs
+++ HimbeertoniRaidTool/Connectors/GearConnector.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using HimbeertoniRaidTool.Data;
 namespace HimbeertoniRaidTool.Connectors
 {
     public interface GearConnector
     {
         public Task<bool> GetGearStats(GearItem item);
+        public Task<bool> GetGearSet(GearSet set);
     }
 }
