6a0265e6958a2b68d059c523fe0a0d53db498bd9
Koenari <Koenari@koenari.de>
2022-02-14 23:28:26 +0100

    Optimization for local sorage of item Values when same item is used twice

--- HimbeertoniRaidTool/Data/GearItem.cs
+++ HimbeertoniRaidTool/Data/GearItem.cs
@@ -1,32 +1,71 @@
-﻿using HimbeertoniRaidTool.Connectors;
-using System;
+﻿using static HimbeertoniRaidTool.Connectors.EtroConnector;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using Dalamud.Logging;
 
 namespace HimbeertoniRaidTool.Data
 {
-    [Serializable]
     public class GearItem
     {
+        private static Dictionary<int, GearItem> Dic = new();
         public int ID { get; init; }
-        public string name { get; set; } = "";
-        public string description { get; set; } = "";
-        public int itemLevel { get; set; } = -1;
+        private string _Name;
+        public string Name { get => _Name; set { _Name = value; AddToDic(); } }
+        private string _Description;
+        public string Description { get => _Description; set { _Description = value; AddToDic(); } }
+        private int _ItemLevel;
+        public int ItemLevel { get => _ItemLevel; set { _ItemLevel = value; AddToDic(); } }
         public GearSource Source { get; set; } = GearSource.undefined;
+        [JsonIgnore]
+        public bool Filled => _ItemLevel > 0 && !_Name.Equals("") && !_Description.Equals("");
+        [JsonIgnore]
+        public bool NeedsStats => ID > 0 && Name == "";
+        [JsonIgnore]
+        public bool Valid => ID > 0;
 
-        public GearItem() : this(-1) { }
+        public static GearItem New(int ID){
+            GearItem? result;
+            Dic.TryGetValue(ID, out result);
+            return result ?? new(ID);
+        }
+        public GearItem() : this(0) { }
 
         public GearItem(int idArg)
         {
             this.ID = idArg;
+            this._Name = "";
+            this._Description = "";
+            this._ItemLevel = 0;
+            GearItem? dicItem;
+            if (Dic.TryGetValue(ID, out dicItem))
+                this.Fill(dicItem);            
         }
 
-        internal void RetrieveItemData(GearConnector con)
+        private void Fill(GearItem dicItem)
         {
-            if(ID > -1)
-                con.GetGearStats(this);
+            if(this.ID == dicItem.ID)
+            {
+                this.Name = dicItem.Name;
+                this.Description = dicItem.Description;
+            }
+        }
+        private void AddToDic()
+        {
+            if (Filled && !Dic.ContainsKey(ID))
+                Dic.Add(ID, this);
+        }
+        internal bool RetrieveItemData()
+        {
+            if (!NeedsStats)
+                return true;
+            GearItem? dicItem;
+            if (Dic.TryGetValue(ID, out dicItem))
+            {
+                this.Fill(dicItem);
+                return true;
+            }
+            return GetGearStats(this);
         }
     }
     public enum GearSource
