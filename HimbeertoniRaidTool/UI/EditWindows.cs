5c45ad327cc94b63bf92c56f26681e685bab4449
Koenari <Koenari@koenari.de>
2022-03-14 14:27:10 +0100

    Internal reorganization to keep show and edit windows for Data classes central

---
+++ HimbeertoniRaidTool/UI/EditWindows.cs
@@ -0,0 +1,124 @@
+﻿using ColorHelper;
+using HimbeertoniRaidTool.Data;
+using ImGuiNET;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using static Dalamud.Localization;
+
+namespace HimbeertoniRaidTool.UI
+{
+    class EditPlayerWindow : HrtUI
+    {
+        private readonly Player Player;
+        private readonly Player PlayerCopy;
+        private bool BISChanged = false;
+        private readonly AsyncTaskWithUiResult CallBack;
+        internal PositionInRaidGroup Pos => Player.Pos;
+
+        internal EditPlayerWindow(out AsyncTaskWithUiResult callBack, RaidGroup group, PositionInRaidGroup pos) : base()
+        {
+            callBack = CallBack = new();
+            Player = group[pos];
+            PlayerCopy = new();
+            if (!Player.Filled && Helper.Target is not null)
+            {
+                PlayerCopy.MainChar.Name = Helper.Target.Name.TextValue;
+                PlayerCopy.MainChar.MainClassType = Helper.TargetClass!;
+            }
+            else if (Player.Filled)
+            {
+                PlayerCopy = Player.Clone();
+            }
+            Show();
+        }
+
+        public override void Draw()
+        {
+            if (!Visible)
+                return;
+            ImGui.SetNextWindowSize(new Vector2(500, 250), ImGuiCond.Always);
+            if (ImGui.Begin(Localize("Edit Player ", "Edit Player ") + Player.Pos,
+                ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar
+                | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                ImGui.InputText(Localize("Player Name", "Player Name"), ref PlayerCopy.NickName, 50);
+                ImGui.InputText(Localize("Character Name", "Character Name"), ref PlayerCopy.MainChar.Name, 50);
+
+                int mainClass = (int)PlayerCopy.MainChar.MainClassType;
+                if (ImGui.Combo(Localize("Main Class", "Main Class"), ref mainClass, Enum.GetNames(typeof(AvailableClasses)), Enum.GetNames(typeof(AvailableClasses)).Length))
+                {
+                    PlayerCopy.MainChar.MainClassType = (AvailableClasses)mainClass;
+                }
+
+                AvailableClasses? curClass = null;
+                if (PlayerCopy.MainChar.Name.Equals(Helper.Target?.Name.TextValue))
+                {
+                    if (Enum.TryParse(Helper.Target!.ClassJob.GameData!.Abbreviation, false, out AvailableClasses parsed))
+                        curClass = parsed;
+                }
+                else if (PlayerCopy.MainChar.Name.Equals(Services.ClientState.LocalPlayer?.Name.TextValue))
+                {
+                    if (Enum.TryParse(Services.ClientState.LocalPlayer!.ClassJob.GameData!.Abbreviation, false, out AvailableClasses parsed))
+                        curClass = parsed;
+                }
+                if (curClass is not null)
+                {
+                    ImGui.SameLine();
+                    if (ImGui.Button(Localize("Current", "Current")))
+                    {
+                        PlayerCopy.MainChar.MainClassType = (AvailableClasses)curClass;
+                    }
+                }
+                if (ImGui.InputText(Localize("BIS", "BIS"), ref Player.MainChar.MainClass.BIS.EtroID, 100))
+                {
+                    BISChanged = true;
+                }
+                ImGui.SameLine();
+                if (ImGui.Button(Localize("Default", "Default") + "##BIS"))
+                {
+                    if (!PlayerCopy.MainChar.MainClass.BIS.EtroID.Equals(HRTPlugin.Configuration.DefaultBIS[Player.MainChar.MainClass.ClassType]))
+                    {
+                        BISChanged = true;
+                        PlayerCopy.MainChar.MainClass.BIS.EtroID = HRTPlugin.Configuration.DefaultBIS[Player.MainChar.MainClass.ClassType];
+                    }
+                }
+                ImGui.SameLine();
+                if (ImGui.Button(Localize("Reset", "Reset") + "##BIS"))
+                {
+                    if (!PlayerCopy.MainChar.MainClass.BIS.EtroID.Equals(""))
+                    {
+                        PlayerCopy.MainChar.MainClass.BIS.EtroID = "";
+                        BISChanged = false;
+                        PlayerCopy.MainChar.MainClass.BIS.Clear();
+                    }
+                }
+                if (ImGui.Button(Localize("Save", "Save")))
+                {
+                    Player.NickName = PlayerCopy.NickName;
+                    Player.MainChar.Name = PlayerCopy.MainChar.Name;
+                    Player.MainChar.MainClassType = PlayerCopy.MainChar.MainClassType;
+                    if (BISChanged)
+                    {
+                        CallBack.Action =
+                            (t) =>
+                            {
+                                if (((Task<bool>)t).Result)
+                                    ImGui.TextColored(HRTColorConversions.Vec4(ColorName.Green),
+                                        $"BIS for {Player.MainChar.Name} ({Player.MainChar.MainClassType}) succesfully updated");
+                                else
+                                    ImGui.TextColored(HRTColorConversions.Vec4(ColorName.Red),
+                                        $"BIS update for {Player.MainChar.Name} ({Player.MainChar.MainClassType}) failed");
+                            };
+                        CallBack.Task = Task.Run(() => EtroConnector.GetGearSet(Player.MainChar.MainClass.BIS));
+                    }
+                    Hide();
+                }
+                ImGui.SameLine();
+                if (ImGui.Button(Localize("Cancel", "Cancel")))
+                    Hide();
+            }
+            ImGui.End();
+        }
+    }
+}
---
+++ HimbeertoniRaidTool/UI/ShowDataWindows.cs
@@ -0,0 +1,46 @@
+﻿using ColorHelper;
+using HimbeertoniRaidTool.Data;
+using ImGuiNET;
+using System.Numerics;
+using static Dalamud.Localization;
+
+namespace HimbeertoniRaidTool.UI
+{
+    public class ShowItemWindow : HrtUI
+    {
+        private readonly GearItem Item;
+        public ShowItemWindow(GearItem item) : base() => (Item, Visible) = (item, true);
+        public override void Draw()
+        {
+            if (!Visible)
+                return;
+            ImGui.SetNextWindowSize(new Vector2(250, 250), ImGuiCond.Always);
+            if (ImGui.Begin(Item.Item.Name, ref Visible,
+                ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar
+                | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+
+                if (ImGui.BeginTable("ItemTable", 2, ImGuiTableFlags.Borders))
+                {
+                    ImGui.TableSetupColumn(Localize("Header", "Header"));
+                    ImGui.TableSetupColumn(Localize("Value", "Value"));
+                    DrawRow(Localize("Name", "Name"), Item.Item.Name);
+                    DrawRow(Localize("Item Level", "Item Level"), Item.ItemLevel.ToString());
+                    DrawRow(Localize("Item Source", "Item Source"), Item.Source.ToString());
+
+                    ImGui.EndTable();
+                }
+            }
+            static void DrawRow(string label, string value)
+            {
+
+                ImGui.TableNextColumn();
+                ImGui.Text(label);
+                ImGui.PushStyleColor(ImGuiCol.TableRowBg, HRTColorConversions.Vec4(ColorName.White, 0.5f));
+                ImGui.TableNextColumn();
+                ImGui.Text(value);
+                ImGui.PopStyleColor();
+            }
+        }
+    }
+}
