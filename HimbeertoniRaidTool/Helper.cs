fe76bc4ab3a088944301f229adea9b7c15fc3ff2
Koenari <Koenari@koenari.de>
2022-03-14 14:12:30 +0100

    Bundle all Helpers together

--- HimbeertoniRaidTool/Helper.cs
+++ HimbeertoniRaidTool/Helper.cs
@@ -1,9 +1,29 @@
-﻿using ColorHelper;
+﻿using HimbeertoniRaidTool.Data;
+using Newtonsoft.Json;
+using System;
 using System.Numerics;
 
-namespace HimbeertoniRaidTool.UI
+namespace HimbeertoniRaidTool
 {
-    public class Helper
+    public static class Helper
+    {
+        public static Dalamud.Game.ClientState.Objects.Types.Character? Target => (Dalamud.Game.ClientState.Objects.Types.Character?)Services.TargetManager.Target;
+        public static AvailableClasses TargetClass => Enum.Parse<AvailableClasses>(Target!.ClassJob.GameData!.Abbreviation.RawString, true);
+    }
+    public static class HRTExtensions
+    {
+        public static T Clone<T>(this T source)
+        {
+            var serialized = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<T>(serialized)!;
+        }
+    }
+
+
+}
+namespace ColorHelper
+{
+    public static class HRTColorConversions
     {
         public static Vector4 Vec4(ColorName name) => Vec4(name.ToRgb());
         public static Vector4 Vec4(ColorName name, float alpha) => Vec4(name.ToRgb(), alpha);
@@ -12,10 +32,6 @@ namespace HimbeertoniRaidTool.UI
         public static Vector4 Vec4(RGB c) => Vec4(c, 1f);
         public static Vector4 Vec4(RGB c, float alpha) => new(c.R / 100f, c.G / 100f, c.B / 100f, alpha);
     }
-
-}
-namespace ColorHelper
-{
     public static class ColorExtensions
     {
         public static HSV Saturation(this HSV hsv, float sat)
