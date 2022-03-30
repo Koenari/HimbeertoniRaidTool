285f67f621efe53473dfd74e2fb42a9c1aac1c12
Koenari <Koenari@koenari.de>
2022-03-30 12:36:29 +0200

    typo

--- HimbeertoniRaidTool/UI/EditWindows.cs
+++ HimbeertoniRaidTool/UI/EditWindows.cs
@@ -1,10 +1,10 @@
-﻿using ColorHelper;
-using HimbeertoniRaidTool.Data;
-using ImGuiNET;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Threading.Tasks;
+using ColorHelper;
+using HimbeertoniRaidTool.Data;
+using ImGuiNET;
 using static Dalamud.Localization;
 
 namespace HimbeertoniRaidTool.UI
@@ -117,11 +117,11 @@ namespace HimbeertoniRaidTool.UI
                                 }
                                 if (success.Length > 0)
                                     ImGui.TextColored(HRTColorConversions.Vec4(ColorName.Green),
-                                            $"BIS for Chracter {Player.MainChar.Name} on classes ({success[0..^1]}) succesfully updated");
+                                            $"BIS for Character {Player.MainChar.Name} on classes ({success[0..^1]}) succesfully updated");
 
                                 if (error.Length > 0)
                                     ImGui.TextColored(HRTColorConversions.Vec4(ColorName.Green),
-                                            $"BIS update for Chracter {Player.MainChar.Name} on classes ({error[0..^1]}) failed");
+                                            $"BIS update for Character {Player.MainChar.Name} on classes ({error[0..^1]}) failed");
                             };
                         CallBack.Task = Task.Run(() => bisUpdates.ConvertAll((x) => (x.Item1, x.Item2.Invoke())));
                     }
