d18c0bd0ae919c0ae4cccf59e1b8d71d6edda267
Koenari <Koenari@koenari.de>
2022-08-26 19:05:39 +0200

    Attempt to automatically fill solo page (if new)

--- HimbeertoniRaidTool/LootMaster/LootMaster.cs
+++ HimbeertoniRaidTool/LootMaster/LootMaster.cs
@@ -9,11 +9,32 @@ namespace HimbeertoniRaidTool.LootMaster
         internal static List<RaidGroup> RaidGroups => DataManagement.DataManager.Groups;
         internal static void Init()
         {
+            bool fillSolo = false;
             if (RaidGroups.Count == 0)
+            {
                 RaidGroups.Add(new("Solo", GroupType.Solo));
+                fillSolo = true;
+            }
             if (RaidGroups[0].Type != GroupType.Solo || !RaidGroups[0].Name.Equals("Solo"))
+            {
                 RaidGroups.Insert(0, new("Solo", GroupType.Solo));
+                fillSolo = true;
+            }
+            if (fillSolo)
+                FillSoloChar();
             GearRefresherOnExamine.Enable();
+        }
+        private static void FillSoloChar()
+        {
+            var character = Helper.Self;
+            if (character == null)
+                return;
+            Player p = RaidGroups[0].Tank1;
+            Character c = new Character(character.Name.TextValue, character.HomeWorld.Id);
+            DataManagement.DataManager.GetManagedCharacter(ref c);
+            p.MainChar = c;
+            c.MainClassType = Helper.GetClass(character) ?? AvailableClasses.AST;
+            c.MainClass.Level = character.Level;
         }
         public static void OnCommand(string args)
         {
