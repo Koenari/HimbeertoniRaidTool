4822ba62de9d19661e724a9c19fa0ba33eead5b6
Koenari <koenari@koenari.de>
2022-09-20 23:22:13 +0200

    feat: add searchable combo field thx to @UnknownX7

--- HimbeertoniRaidTool/UI/ImGuiHelper.cs
+++ HimbeertoniRaidTool/UI/ImGuiHelper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using Dalamud.Interface;
 using HimbeertoniRaidTool.Data;
 using HimbeertoniRaidTool.Modules.LootMaster;
 using ImGuiNET;
+using Lumina.Excel;
 using static Dalamud.Localization;
 
 namespace HimbeertoniRaidTool.UI
@@ -74,6 +76,75 @@ namespace HimbeertoniRaidTool.UI
             if (result)
                 value = (T?)Enum.GetValues(typeof(T)).GetValue(id);
             return result;
+        }
+        //Credit to UnknownX
+        //Modified to have filtering of Excel sheet and be usable by keayboard only
+        public static bool ExcelSheetCombo<T>(string id, out T? selected, Func<ExcelSheet<T>, string> getPreview, ImGuiComboFlags flags, Func<T, string, bool> searchPredicate, Func<T, string> toName, Func<T, bool> preFilter) where T : ExcelRow
+        {
+            ExcelSheet<T>? sheet = Services.DataManager.GetExcelSheet<T>();
+            if (sheet is null)
+            {
+                selected = null;
+                return false;
+            }
+            return ExcelSheetCombo(id, out selected, getPreview(sheet), flags, sheet, searchPredicate, toName, preFilter);
+        }
+
+        private static string search = string.Empty;
+        private static HashSet<ExcelRow>? filtered;
+        private static int hoveredItem = 0;
+        //This is a small hack since to my knowledge there is no way to close and existing combo when not clikced
+        private static bool toogle = false;
+        private static bool wasEnterClickedLastTime = false;
+        public static bool ExcelSheetCombo<T>(string id, out T? selected, string preview, ImGuiComboFlags flags, ExcelSheet<T> sheet, Func<T, string, bool> searchPredicate, Func<T, string> toName, Func<T, bool> preFilter) where T : ExcelRow
+        {
+            if (wasEnterClickedLastTime)
+            {
+                toogle = !toogle;
+                search = string.Empty;
+                filtered = null;
+            }
+            bool enterClicked = ImGui.IsKeyPressed(ImGuiKey.Enter);
+            wasEnterClickedLastTime = enterClicked;
+            if (ImGui.IsKeyPressed(ImGuiKey.UpArrow))
+                hoveredItem--;
+            if (ImGui.IsKeyPressed(ImGuiKey.DownArrow))
+                hoveredItem++;
+            hoveredItem = Math.Clamp(hoveredItem, 0, filtered?.Count ?? 0);
+            selected = null;
+            if (!ImGui.BeginCombo(id + (toogle ? "##x" : ""), preview, flags)) return false;
+
+            if (ImGui.IsWindowAppearing() && ImGui.IsWindowFocused() && !ImGui.IsAnyItemActive())
+            {
+                search = string.Empty;
+                filtered = null;
+                ImGui.SetKeyboardFocusHere(0);
+            }
+
+            if (ImGui.InputText("##ExcelSheetComboSearch", ref search, 128))
+                filtered = null;
+            if (filtered == null)
+            {
+                filtered = sheet.Where(preFilter).Where(s => searchPredicate(s, search)).Cast<ExcelRow>().ToHashSet();
+                hoveredItem = 0;
+            }
+            int i = 0;
+            foreach (var row in filtered.Cast<T>())
+            {
+                bool hovered = hoveredItem == i;
+                ImGui.PushID(i);
+
+                if (ImGui.Selectable(toName(row), hovered) || (enterClicked && hovered))
+                    selected = row;
+                ImGui.PopID();
+                i++;
+                if (selected == null) continue;
+                ImGui.EndCombo();
+                return true;
+            }
+
+            ImGui.EndCombo();
+            return false;
         }
     }
 }
