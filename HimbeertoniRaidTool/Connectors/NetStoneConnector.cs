1f9d414f7867bf6946c4617034b3003cbd665dee
ge7nic <48736317+ge7nic@users.noreply.github.com>
2022-09-25 14:51:56 +0200

    Add NetStoneConnector

---
+++ HimbeertoniRaidTool/Connectors/NetStoneConnector.cs
@@ -0,0 +1,27 @@
+﻿using NetStone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HimbeertoniRaidTool.Connectors
+{
+    internal static class NetStoneConnector
+    {
+        private static LodestoneClient? lodestoneClient;
+
+        public static async Task DebugToConsole(string charname)
+        {
+            lodestoneClient ??= await LodestoneClient.GetClientAsync();
+            try
+            {
+                Dalamud.Logging.PluginLog.Log("Got Client with type {0}.", lodestoneClient.GetType());
+            }
+            catch
+            {
+                Dalamud.Logging.PluginLog.Error("Something went wrong with fetching Data from Lodestone.");
+            }
+        }
+    }
+}
