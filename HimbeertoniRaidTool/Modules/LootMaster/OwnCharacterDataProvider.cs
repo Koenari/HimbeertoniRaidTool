675975555dcfd3839717c48a4231e65035fd0b62
Koenari <koenari@koenari.de>
2022-12-22 16:24:27 +0100

    adapt to new project structure

--- HimbeertoniRaidTool/Modules/LootMaster/OwnCharacterDataProvider.cs
+++ HimbeertoniRaidTool/Modules/LootMaster/OwnCharacterDataProvider.cs
@@ -2,45 +2,44 @@
 using System.Diagnostics.CodeAnalysis;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using FFXIVClientStructs.FFXIV.Client.Game;
-using HimbeertoniRaidTool.Data;
+using HimbeertoniRaidTool.Common.Data;
 
-namespace HimbeertoniRaidTool.Modules.LootMaster
+namespace HimbeertoniRaidTool.Plugin.Modules.LootMaster;
+
+internal static class OwnCharacterDataProvider
 {
-    internal static class OwnCharacterDataProvider
+    public static readonly HashSet<Currency> _trackedCurrencies = new()
     {
-        public static readonly HashSet<Currency> _trackedCurrencies = new()
-        {
-            Currency.Gil,
-            Currency.TomestoneOfAstronomy,
-            Currency.TomestoneOfCausality
-        };
-        public static readonly HashSet<uint> _trackedItems = new()
-        {
+        Currency.Gil,
+        Currency.TomestoneOfAstronomy,
+        Currency.TomestoneOfCausality
+    };
+    public static readonly HashSet<uint> _trackedItems = new()
+    {
 
-        };
-        private static bool GetChar([NotNullWhen(true)] out Character? target, [NotNullWhen(true)] out PlayerCharacter? source)
-        {
-            target = null;
-            source = Services.ClientState.LocalPlayer;
-            if (source == null)
-                return false;
+    };
+    private static bool GetChar([NotNullWhen(true)] out Character? target, [NotNullWhen(true)] out PlayerCharacter? source)
+    {
+        target = null;
+        source = Services.ClientState.LocalPlayer;
+        if (source == null)
+            return false;
 
-            target = new(source.Name.TextValue, source.HomeWorld.Id);
-            return Services.HrtDataManager.GetManagedCharacter(ref target);
-        }
-        public static unsafe void UpdateWallet()
+        target = new(source.Name.TextValue, source.HomeWorld.Id);
+        return Services.HrtDataManager.GetManagedCharacter(ref target);
+    }
+    public static unsafe void UpdateWallet()
+    {
+        if (!GetChar(out Character? target, out PlayerCharacter? source))
+            return;
+        InventoryContainer* container = InventoryManager.Instance()->GetInventoryContainer(InventoryType.Currency);
+        for (int i = 0; i < container->Size; i++)
         {
-            if (!GetChar(out Character? target, out PlayerCharacter? source))
-                return;
-            InventoryContainer* container = InventoryManager.Instance()->GetInventoryContainer(InventoryType.Currency);
-            for (int i = 0; i < container->Size; i++)
+            var item = container->Items[i];
+            Currency type = (Currency)item.ItemID;
+            if (_trackedCurrencies.Contains(type))
             {
-                var item = container->Items[i];
-                Currency type = (Currency)item.ItemID;
-                if (_trackedCurrencies.Contains(type))
-                {
-                    target.Wallet[type] = item.Quantity;
-                }
+                target.Wallet[type] = item.Quantity;
             }
         }
     }
