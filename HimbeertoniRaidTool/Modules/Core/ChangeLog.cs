4e0f0b8fe6d4d9eb352bb110900ba4e1d087b054
Koenari <koenari@koenari.de>
2024-02-19 18:46:44 +0100

    build: 1.5.2.5

--- HimbeertoniRaidTool/Modules/Core/ChangeLog.cs
+++ HimbeertoniRaidTool/Modules/Core/ChangeLog.cs
@@ -6,7 +6,7 @@ public class ChangeLog
 {
     public static readonly IReadOnlyList<SingleVersionChangelog> Entries = new List<SingleVersionChangelog>
     {
-        new(new Version(1, 5, 2, 4))
+        new(new Version(1, 5, 2, 5))
         {
             MinorFeatures =
             {
